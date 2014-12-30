d918668519f81873bb77a82daefe17313d95b4da
Bert Spaan <bert.spaan@gmail.com>
2014-12-30 12:50:00 +0100

    relative paths!

--- lib/citysdk.rb
+++ lib/citysdk.rb
@@ -1,7 +1,7 @@
-require 'citysdk/util.rb'
-require 'citysdk/api.rb'
-require 'citysdk/file_reader.rb'
-require 'citysdk/importer.rb'
+require_relative './citysdk/util.rb'
+require_relative './citysdk/api.rb'
+require_relative './citysdk/file_reader.rb'
+require_relative './citysdk/importer.rb'
 
 module CitySDK
   VERSION = "1.0"
